be17dba0f75b61c6b3ec5adca7a931ad4b686e11
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-07-26 20:37:56 +0300

    Add files via upload

--- image-images/index.js
+++ image-images/index.js
@@ -3,23 +3,21 @@ var sharp = require('sharp')
 
 // urmarim schimbarile din dosarul images/original
 
-fs.watch("./images/original", function(){
+fs.watch("./images/original", function() {
     console.log("Something has changed!")
-    //fs ->lista fisierelor dintr-un directoriu
-  var file_names = fs.readdirSync("./images/original")
-
-  //array of string!!!
-  //prin metodele string (to...)->testati extensiile fisierilor originale(.jpg , .png) toate celelate extensii ignorate
-  //in functie genThumb
-  //toate pozele convertite sa fie impartite in 2 mape diferite (thumbs/jpeg) (thumbs/png)
-  //endsWith()
-  file_names.forEach( genThumb )
-  //console.log(file_names)
+        //fs ->lista fisierelor dintr-un directoriu
+    var file_names = fs.readdirSync("./images/original")
+    file_names.forEach(genThumb)
 })
 
-
-function genThumb( file_name ){
-      sharp (`./images/original/${file_name}`)
-     .resize(100,100,)
-     .toFile(`./images/thumbs/-sm-${file_name}`)
+function genThumb(file_name) {
+    if (`${file_name}`.endsWith("jpg")) {
+        sharp(`./images/original/${file_name}`)
+            .resize(200, 200)
+            .toFile(`./images/thumbs/jpg/-sm-${file_name}`)
+    } else {
+        sharp(`./images/original/${file_name}`)
+            .resize(200, 200)
+            .toFile(`./images/thumbs/png/-xm-${file_name}`)
+    }
 }
