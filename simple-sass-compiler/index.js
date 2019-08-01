d104d97868c57698660e991a9e2df780c3df95a5
denisciumac1 <denisciumac@gmail.com>
2019-08-01 14:39:27 +0300

    +autocompile+compressed

--- simple-sass-compiler/index.js
+++ simple-sass-compiler/index.js
@@ -1,19 +1,20 @@
-var fs = require('fs');
-var sass = require('node-sass');
+var fs = require('fs')
+var sass = require('node-sass')
+
+////////////////Autocompilare//////////////////////////
+fs.watchFile("./src/style.scss", function() {
+        console.log("Something has changed!")
+        var file_change = fs.readFileSync("./src/style.scss")
+        file_change.forEach(compileSCSS)
+    })
+    ////////////////Autocompilare//////////////////////////
 
 function compileSCSS() {
-    var result = sass.render({
+    var result = sass.renderSync({
             file: './src/style.scss',
-            outFile: './dist/style.css',
+            outputStyle: 'compressed', // Stilul compresat!
 
-        },
-        function(error, result) {
-            if (error) {
-                console.log(error)
-            } else {
-                fs.writeFile(result)
-            }
         })
-
+        // console.log(result.css.toString())
+    fs.writeFileSync("./dist/style.css", result.css.toString())
 }
-compileSCSS()
