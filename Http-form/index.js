603ea1b73c4bec21fb4dfd4cd25709ccd6eaf9db
denisciumac1 <denisciumac@gmail.com>
2019-08-08 12:16:14 +0300

    array

--- Http-form/index.js
+++ Http-form/index.js
@@ -2,39 +2,35 @@ var http = require("http")
 var fs = require("fs")
 var querystring = require("querystring")
 
-var server = http.createServer( (req, res) =>{
+var server = http.createServer((req, res) => {
 
-if (req.url=="/"){
-    res.write(fs.readFileSync("form.html"))
-}else if(req.url.indexOf("?email=") !=-1){
- var url_data = querystring.parse(req.url)
- console.log(url_data['/?email'])
+    if (req.url == "/") {
+        res.write(fs.readFileSync("form.html"))
+    } else if (req.url.indexOf("?email=") != -1) {
+        var url_data = querystring.parse(req.url)
+        console.log(url_data['/?email'])
 
- ////////////save new address////////////
- var data = JSON.parse(fs.readFileSync("./emails.json")) // array de date din fisier 
- if(!data.includes( url_data['/?email'] )){
-    data.push( url_data['/?email'] )
- 
-    fs.writeFileSync("./emails.json", JSON.stringify(data))
- }else if(req.url == "/send"){
-    var data = JSON.parse(fs.readFileSync("./emails.json"))
-    data.forEach(email => {
-        res.write(`${email}<br>`)
-    });
-     res.write("Sending automated emails!")
-     // utilizind nodemailer sa se trimite cite un mesaj pe fiecare adresa din acea lista
- }
- 
+        ////////////save new address////////////
+        var data = JSON.parse(fs.readFileSync("./emails.json")) // array de date din fisier 
 
- ///////////////////////////////////////
+        if (!data.includes(url_data['/?email'])) {
+            data.push(url_data['/?email'])
+            fs.writeFileSync("./emails.json", JSON.stringify(data))
+        } else if (req.url == "/send") {
+            var data = JSON.parse(fs.readFileSync("./emails.json"))
+            data.forEach(email => {
+                res.write(`${email}<br>`)
+            });
+            res.write("Sending automated emails!")
+                // utilizind nodemailer sa se trimite cite un mesaj pe fiecare adresa din acea lista
+        }
 
+        ///////////////////////////////////////
 
- res.write(fs.readFileSync("thanks.html"))
-}
+        res.write(fs.readFileSync("thanks.html"))
+    }
 
-
-
-res.end()
+    res.end()
 
 })
 
--- http-server/index.js
+++ http-server/index.js
@@ -6,11 +6,16 @@ let server = http.createServer((req, res) => {
     console.log(req.url)
     if (req.url == "/") {
         var html = fs.readFileSync("./html/home.html")
-    } else if (req.url == "/css/style.css") {
-        var html = fs.readFileSync("./css/style.css")
-    } else if (req.url == "/css/animation.css") {
-        var html = fs.readFileSync("./css/animation.css")
-    } else if (req.url == "/images/icon.png") {
+    } else if (req.url.endsWith(".css")) {
+        var html = fs.readFileSync("." + req.url)
+    }
+
+    // else if (req.url == "/css/style.css") {
+    //     var html = fs.readFileSync("./css/style.css")
+    // } else if (req.url == "/css/animation.css") {
+    //     var html = fs.readFileSync("./css/animation.css")
+    // } 
+    else if (req.url == "/images/icon.png") {
         var html = fs.readFileSync("./images/icon.png")
     } else {
         var html = "Not found"
